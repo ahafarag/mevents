ce2de1d2cb5e42a2cfe9f79fc0121d5d0b30a8d7
ahafarag <abdelrahmanfarag1980@gmail.com>
2025-06-15 23:35:46 +0200

    Add AdminLTE layout and user management

--- prize-draw-app/resources/views/layouts/app.blade.php
+++ prize-draw-app/resources/views/layouts/app.blade.php
@@ -14,37 +14,81 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js']) {{-- For Vite --}}
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
-<body class="hold-transition sidebar-mini"> {{-- AdminLTE body classes --}}
-    <div id="app">
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <!-- Navbar -->
         <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-            {{-- Navbar content --}}
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="{{ url('/home') }}" class="nav-link">Home</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+            </ul>
         </nav>
+        <!-- /.navbar -->
 
+        <!-- Main Sidebar Container -->
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            {{-- Sidebar content --}}
-        </aside>
+            <a href="{{ url('/home') }}" class="brand-link">
+                <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
+            </a>
 
-        <div class="content-wrapper">
-            <div class="content-header">
-                <div class="container-fluid">
-                    <div class="row mb-2">
-                        <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
-                        </div>
+            <div class="sidebar">
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <i class="fas fa-user-circle text-white"></i>
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">{{ Auth::user()->name ?? 'Guest' }}</a>
                     </div>
                 </div>
+
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" role="menu">
+                        <li class="nav-item">
+                            <a href="{{ url('/home') }}" class="nav-link">
+                                <i class="nav-icon fas fa-home"></i>
+                                <p>Dashboard</p>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('users.index') }}" class="nav-link">
+                                <i class="nav-icon fas fa-users"></i>
+                                <p>Users</p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-            <div class="content">
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <section class="content pt-3">
                 <div class="container-fluid">
                     @yield('content')
                 </div>
-            </div>
+            </section>
         </div>
 
-        <footer class="main-footer">
-            {{-- Footer content --}}
+        <footer class="main-footer text-sm">
+            <div class="float-right d-none d-sm-inline">AdminLTE</div>
+            <strong>&copy; 2024</strong>
         </footer>
     </div>
 </body>
